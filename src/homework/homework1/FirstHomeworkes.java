6525843544a814ef6460c97045d376dba2bbaada
Sargis <mkrtchyansargis395@gmail.com>
2025-09-09 00:09:05 +0400

    homework 1

---
+++ src/homework/homework1/FirstHomeworkes.java
@@ -0,0 +1,32 @@
+package homework.homework1;
+
+public class FirstHomeworkes {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 4;
+        if (a > b) {
+            System.out.println(" ");
+            System.out.println("true");
+        }
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("");
+            System.out.print(i + " ");
+        }
+
+        int c = 5;
+        int l = 7;
+        int sum = c + l;
+        System.out.println(" ");
+        System.out.print("գումարը = " + sum);
+
+
+        int n = 3;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(" ");
+            System.out.println(n + "*" + i + "=" + (n * i));
+        }
+
+
+    }
+}
