dbbf1b01ea90630cbfc3715c0a466c431de200db
Sargis <mkrtchyansargis395@gmail.com>
2025-10-07 18:25:05 +0400

    DynamicArray

--- src/homework/dynamicarray/DynamicArray.java
+++ src/homework/dynamicarray/DynamicArray.java
@@ -34,4 +34,64 @@ public class DynamicArray {
         System.out.println();
     }
 
+    //ջնջել մասիվի index-երորդ էլեմենտը
+//եթե չկա նման ինդեքսով էլեմենտ, տպել, որ չկա նման էլեմենտ
+    public void deleteByIndex(int index) {
+        if (index < 0 || index > size) {
+            System.out.println("չկա նման էլեմենտ");
+            return;
+        }
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        size--;
+
+    }
+
+    //տրված value-ն դնելու ենք տրված index-ով էլեմենտի տեղը։
+//Հին արժեքը կկորի
+//եթե նման ինդեքսով էլեմենտ չկա, գրում ենք որ չկա։
+    public void set(int index, int value) {
+        if (index < 0 || index > size) {
+            System.out.println("չկա նման էլեմենտ");
+        } else {
+            array[index] = value;
+        }
+    }
+
+    //ավելացնել տրված value-ն տրված ինդեքսում, իսկ էղած էլեմենտները մի հատ աջ տանել։
+//Եթե չկա նման ինդեքս, տպել որ չկա
+    public void add(int index, int value) {
+        if (index < 0 || index > size) {
+            System.out.println("չկա նման էլեմենտ");
+        } else {
+            for (int i = size; i > index; i--) {
+                array[i] = array[i - 1];
+            }
+            array[index] = value;
+            size++;
+        }
+    }
+
+    //Վերադարձնել true եթե տրված value-ն կա մեր մասիվի մեջ, եթե ոչ false
+    public boolean exists(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Վերադարձնել տրված value-ի ինդեքսը, եթե շատ կա տվյալ թվից, վերադարձնել առաջին ինդեքսը։
+//եթե չկա, -1
+    public int getIndexByValue(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
--- src/homework/dynamicarray/DynamicArrayDemo.java
+++ src/homework/dynamicarray/DynamicArrayDemo.java
@@ -4,16 +4,15 @@ public class DynamicArrayDemo {
     public static void main(String[] args) {
 
         DynamicArray dynamicArray = new DynamicArray();
-
-        for (int i = 0; i < 10; i++) {
-            dynamicArray.add(i * 2);
-        }
-
-        System.out.print("array = ");
+        dynamicArray.add(70);
+        dynamicArray.add(30);
+        dynamicArray.add(80);
+        dynamicArray.add(20);
+        dynamicArray.deleteByIndex(2);
+        System.out.println(dynamicArray.getByIndex(1));
+        dynamicArray.add(3,15);
+        System.out.println(dynamicArray.exists(2));
+        System.out.println(dynamicArray.getIndexByValue(30));
         dynamicArray.print();
-
-        System.out.println("since 5 elements " + dynamicArray.getByIndex(1));
-        System.out.println("since 10 elements " + dynamicArray.getByIndex(10));
-
     }
 }
